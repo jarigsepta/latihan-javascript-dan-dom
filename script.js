682ee4c3075aac659a8fd157a2e84a5e763ffe10
jarigsepta <jarigsepta8@gmail.com>
2020-03-10 20:02:55 +0700

    added query selector

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ h1.style.fontSize = '50px';
 //document.getElementsByClassName -> HTML Collection
 const p1 = document.getElementsByClassName('p1')[0];
 p1.innerHTML = 'Ini diubah dari Javascript';
+
+//document.querySelector() -> element
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+// document.querySelectorAll()
+// const p = document.querySelectorAll('p');
+// for (let i = 0; i < p.length; i++) {
+//     p[1].style.backgroundColor = 'green';
+// }
