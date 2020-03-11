96c8329cc86ee34b5e9c5a63f1ff38ab0b0edc46
jarigsepta <jarigsepta8@gmail.com>
2020-03-11 07:59:50 +0700

    added dom manipulation

---
+++ script1.js
@@ -0,0 +1,18 @@
+//element.innerHTML
+const judul = document.getElementById('judul');
+judul.innerHTML = 'Ini judul baru';
+
+const sectionA = document.querySelector('section#a');
+sectionA.innerHTML = '<div><p>Paragraf</p></div>';
+
+// Element.style.<propertiCSS>
+const judul = document.querySelector('#judul');
+judul.style.color = 'lightblue';
+judul.style.backgroundColor = 'salmon';
+
+//element.setAttribute()
+const judul = document.getElementsByTagName('h1')[0];
+judul.setAttribute('name', 'judul baru');
+
+//element.classList
+const p2 = document.querySelector('.p2');
