b36df7f2794acc5c59f82b33afde3580ed377461
jarigsepta <jarigsepta8@gmail.com>
2020-03-18 20:05:24 +0700

    modified element.style

--- Dom Manipulation/script.js
+++ Dom Manipulation/script.js
@@ -6,13 +6,13 @@ const sectionA = document.querySelector('section#a');
 sectionA.innerHTML = '<div><p>Paragraf</p></div>';
 
 // Element.style.<propertiCSS>
-// const judul = document.querySelector('#judul');
-// judul.style.color = 'lightblue';
-// judul.style.backgroundColor = 'salmon';
+const judul = document.querySelector('#judul');
+judul.style.color = 'lightblue';
+judul.style.backgroundColor = 'salmon';
 
 //element.setAttribute()
-// const judul = document.getElementsByTagName('h1')[0];
-// judul.setAttribute('name', 'judul baru');
+const judul = document.getElementsByTagName('h1')[0];
+judul.setAttribute('name', 'judul baru');
 
 //element.classList
 const p2 = document.querySelector('.p2');
