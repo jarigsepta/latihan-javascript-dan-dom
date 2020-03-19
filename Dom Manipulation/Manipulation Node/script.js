d537635a5f31e83110ad61bfc1d9804ef9dc204a
jarigsepta <jarigsepta8@gmail.com>
2020-03-19 16:42:06 +0700

    added replace child

--- Dom Manipulation/Manipulation Node/script.js
+++ Dom Manipulation/Manipulation Node/script.js
@@ -30,3 +30,20 @@ ul.insertBefore(liBaru, li2);
 // removeChild
 const link = document.getElementsByTagName('a')[0];
 sectionA.removeChild(link);
+
+
+//replaceChild
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const teksH2Baru = document.createTextNode('Judul Baru');
+
+h2Baru.appendChild(teksH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+
+pBaru.style.backgroundColor = 'lightgreen';
+liBaru.style.backgroundColor = 'lightgreen';
+h2Baru.style.backgroundColor = 'lightgreen';
