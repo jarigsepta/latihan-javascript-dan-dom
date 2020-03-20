f4f517043df4e0c390b8b6674ba8822ef8f2abba
jarigsepta <jarigsepta8@gmail.com>
2020-03-20 08:50:25 +0700

    added mouse enter & leave

--- Dom Events/script.js
+++ Dom Events/script.js
@@ -23,3 +23,15 @@ p4.addEventListener('click', function () {
     liBaru.appendChild(teksLiBaru);
     ul.appendChild(liBaru);
 })
+
+
+// Mouse enter & Mouse leave
+const p1 = document.querySelector('.p1');
+
+p1.addEventListener('mouseenter', function () {
+    p1.style.backgroundColor = 'lightblue';
+})
+
+p1.addEventListener('mouseleave', function () {
+    p1.style.backgroundColor = 'lightgreen';
+})
