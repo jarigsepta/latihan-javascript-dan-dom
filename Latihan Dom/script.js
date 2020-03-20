6f3af01fbc7daed487d0880050abed90365cb4c5
jarigsepta <jarigsepta8@gmail.com>
2020-03-20 13:29:01 +0700

    added acak warna

--- Latihan Dom/script.js
+++ Latihan Dom/script.js
@@ -5,3 +5,17 @@ tUbahWarna.onclick = function() {
   //document.body.setAttribute("class", "biru-muda");
   document.body.classList.toggle("biru-muda");
 };
+
+// Acak Warna
+const tAcakWarna = document.createElement("button");
+const teksTombol = document.createTextNode("Acak Warna");
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute("type", "button");
+tUbahWarna.after(tAcakWarna);
+
+tAcakWarna.addEventListener("click", function() {
+  const r = Math.round(Math.random() * 255 + 1);
+  const g = Math.round(Math.random() * 255 + 1);
+  const b = Math.round(Math.random() * 255 + 1);
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + ", " + b + ")";
+});
