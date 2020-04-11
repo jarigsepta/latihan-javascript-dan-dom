6208f7b733240b7e016b8b2dc518e2d24babd905
jarigsepta <jarigsepta8@gmail.com>
2020-04-11 19:46:04 +0700

    added latihan

--- latihan/script.js
+++ latihan/script.js
@@ -1,9 +1,21 @@
+// setTimeout()
 const tes = setTimeout(function () {
-    console.log('ok');
+  console.log("ok");
 }, 5000);
 
-const tombol = document.getElementById('tombol');
-tombol.addEventListener('click', function () {
-    clearTimeout(tes);
-    console.log('selesai');
-})
+const tombol = document.getElementById("tombol");
+tombol.addEventListener("click", function () {
+  clearTimeout(tes);
+  console.log("selesai");
+});
+
+// setInterval
+const coba = setInterval(function () {
+  console.log("ok");
+}, 2000);
+
+const klik = document.getElementById("klik");
+klik.addEventListener("click", function () {
+  clearInterval(coba);
+  console.log("selesai");
+});
