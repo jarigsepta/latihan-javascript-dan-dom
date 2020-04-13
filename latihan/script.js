5763cce7ab5901b17a6857ee2b0b744880fd5e35
jarigsepta <jarigsepta8@gmail.com>
2020-04-13 18:54:45 +0700

    added latihan dom

--- latihan/script.js
+++ latihan/script.js
@@ -19,3 +19,31 @@ klik.addEventListener("click", function () {
   clearInterval(coba);
   console.log("selesai");
 });
+
+// Program Hitung Mundur
+const tanggalTujuan = new Date("Apr 13, 2020 16:00:00").getTime();
+const sekarang = new Date().getTime();
+const hitungMundur = setInterval(function () {
+  const selisih = tanggalTujuan - sekarang;
+  const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+  const jam = Math.floor((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const menit = Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60));
+  const detik = Math.floor((selisih % (1000 * 60)) / 1000);
+
+  const teks = document.getElementById("teks");
+  teks.innerHTML =
+    "Waktu anda tinggal : " +
+    hari +
+    " hari " +
+    jam +
+    " jam " +
+    menit +
+    " menit " +
+    detik +
+    " detik lagi ";
+
+  if (selisih == 0) {
+    clearInterval(hitungMundur);
+    teks.innerHTML = "Waktu Habis";
+  }
+}, 1000);
